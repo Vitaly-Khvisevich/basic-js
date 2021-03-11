c48a0d5abadf59c34d466ce85f6e8823fe41fbe3
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-03-11 15:29:22 +0300

    init: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+if (Array.isArray(arr)){
+  y=arr.reduce(function(answ, item, index){
+    if(arr[index]=='--discard-prev'||arr[index]=='--discard-next'||arr[index]=='--double-next'||arr[index]=='--double-prev'){
+      return answ;
+    }
+    if (arr[index - 1] == '--discard-next') {
+      return answ;
+    }
+    if (arr[index - 1] == '--double-next') {
+      answ.push(item);
+    }
+    answ.push(item);
+    if (arr[index + 1] == '--double-prev'){
+      answ.push(item); 
+    }
+    if (arr[index + 1] == '--discard-prev') {
+     answ.splice(answ.length - 1, 1);
+    }
+   return answ;
+  },[]);
+  return(y);
+  }
+else{
+  throw new Error('not an Array!');
+}
 };
