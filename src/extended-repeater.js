b3a22ca694ebb831c1020c61dc0db94d18c2c14e
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-03-12 19:48:41 +0300

    init: solved extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+options.addition = String(options.addition) !== 'undefined' ? String(options.addition) : ''
+options.separator = options.separator ? options.separator : '+'
+options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|'
+let new_str='', part_str='';
+
+if (options.additionRepeatTimes){
+  for(i=0; i<options.additionRepeatTimes; i++){
+    if (i==options.additionRepeatTimes-1){
+      part_str+= options.addition
+    }
+    else{
+      part_str+= options.addition + options.additionSeparator;
+    }   
+  }
+}
+else{ 
+part_str+=options.addition;
+}  
+  
+if (options.repeatTimes){
+  for(j=0; j<options.repeatTimes; j++){
+    if (j==options.repeatTimes-1){
+   new_str+=str+part_str;
+    }
+    else{
+      new_str+=str+part_str+options.separator;
+    }
+  }
+return new_str;
+}
+else{
+ return str + part_str;
+}
 };
   
